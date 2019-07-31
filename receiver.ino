04086992a67a22f7158c5629d8401b9c3843aabf
njrebelo <47528769+njrebelo@users.noreply.github.com>
2019-07-31 22:07:03 +0100

    Update receiver.ino

--- receiver.ino
+++ receiver.ino
@@ -1,17 +1,45 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
+#include <Servo.h>
+
 RF24 radio(7, 8); // CE, CSN
 const byte address[6] = "00001";
 
+Servo myservo;
+Servo myservo1;
+Servo myservo2;
+Servo myservo3;
+Servo myservo4;
+int val1,val2,val3,val4,val5;
+
 void setup() {
+  Serial.begin(9600);
   radio.begin();
-  radio.openWritingPipe(address);
+  radio.openReadingPipe(0, address);
   radio.setPALevel(RF24_PA_MIN);
-  radio.stopListening();
+  radio.startListening();
+  myservo.attach(10);  // attaches the servo on pin 9 to the servo object
+  myservo1.attach(9);
+  myservo2.attach(6);
+  myservo3.attach(5);
+  myservo4.attach(3);
 }
 void loop() {
-  const char text[] = "Hello World";
-  radio.write(&text, sizeof(text));
-  delay(1000);
+  if (radio.available()) {
+    char text[5] = "";
+    radio.read(&text, sizeof(text));
+    Serial.println(text);
+    myservo.write(text[1]);             
+    delay(15);
+    myservo1.write(text[2]);              
+    delay(15);   
+    myservo2.write(text[3]);              
+    delay(15);   
+    myservo3.write(text[4]);              
+    delay(15);   
+    myservo4.write(text[5]); 
+
+   
+  }
 }
